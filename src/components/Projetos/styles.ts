fce8ac5f51184d48e4002f52d0c895292dd74bc6
Derik Oliveira <derikdantas@hotmail.com>
2021-09-02 20:55:06 -0300

    Ajustes de CSS

--- src/components/Projetos/styles.ts
+++ src/components/Projetos/styles.ts
@@ -5,7 +5,8 @@ export const Container = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    height: 100vh;
+    height: auto;
+    margin: 50px;
 
     p{
         text-align: center;
@@ -14,7 +15,6 @@ export const Container = styled.div`
 
     a{
         color: ${({ theme }) => theme.text};
-        text-decoration: none;
 
         &:hover{
             text-decoration: underline;
